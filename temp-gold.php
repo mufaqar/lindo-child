bc605b613d77da457d7f3ce89fa7aee53e267c0b
Mufaqar <mufaqar@gmail.com>
2026-03-09 14:58:38 +0500

    updateg

--- temp-gold.php
+++ temp-gold.php
@@ -8,19 +8,7 @@ get_header();
 
 
 
-
-$url = "https://api.metals.dev/v1/latest?api_key=QWOIJUPFGXI8MSHKQLZW205HKQLZW&currency=PKR&unit=g";
-
-$curl = curl_init($url);
-curl_setopt($curl, CURLOPT_URL, $url);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-$headers = array("Accept: application/json");
-curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-
-$resp = curl_exec($curl);
-curl_close($curl);
-var_dump($resp);
+pmx_fetch_and_store_gold_rates();
 
 
 
